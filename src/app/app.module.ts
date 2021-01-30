ea5fdb31d7ff2b80fae71d4cbaf6c14524fa901a
light86neon <72137342+light86neon@users.noreply.github.com>
2021-01-30 10:21:02 +0200

    add hw1 again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { CarComponent } from './components/car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/data/cars.ts
@@ -0,0 +1,5 @@
+export const cars = [
+  {name: 'Audi', year: 2010, engine: 3.0},
+  {name: 'BMW', year: 2012, engine: 2.0},
+  {name: 'Skoda', year: 2014, engine: 1.6},
+];
